e22c1e3e079e863f06237c87a8c281044e61194d
Logan <lupenoxwolf@gmail.com>
2026-06-06 12:43:17 -0500

    Register app models in Django admin

--- project_app/admin.py
+++ project_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from project_app.models import ContactInfo, Course, Section, User
+
+
+admin.site.register(ContactInfo)
+admin.site.register(Course)
+admin.site.register(Section)
+admin.site.register(User)
